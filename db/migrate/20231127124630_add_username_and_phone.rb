22e2f2de51eaeee3660c7666601a9ad1df304c92
Riru Mizutani <seahorse3106@gmail.com>
2023-11-27 22:36:55 +0900

    add column

--- db/migrate/20231127124630_add_username_and_phone.rb
+++ db/migrate/20231127124630_add_username_and_phone.rb
@@ -1,5 +1,6 @@
 class AddUsernameAndPhone < ActiveRecord::Migration[7.1]
   def change
-    add_column :user, :username, :string, :phone, :string
+    add_column :users, :username, :string 
+    add_column :users, :phone, :string
   end
 end
