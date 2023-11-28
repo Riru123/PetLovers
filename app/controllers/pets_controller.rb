8e12510103626dce3c35f0fa98df758ab5f66832
Riru Mizutani <seahorse3106@gmail.com>
2023-11-29 00:06:39 +0900

    create action & new page is done

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,5 @@
 class PetsController < ApplicationController
+    
     skip_before_action :authenticate_user!, only: [:index, :show]
     def index
         @pets = Pet.all
@@ -7,5 +8,23 @@ class PetsController < ApplicationController
     def show
         @pet = Pet.find(params[:id])
     end
+
+    def new
+        @pet = Pet.new
+      end
     
+      def create
+        @pet = Pet.new(pet_params)
+        @pet.user = current_user
+        if @pet.save
+          redirect_to pet_path(@pet)
+        else
+          render :new, status: :unprocessable_entity
+        end
+    end
+    
+    private
+    def pet_params
+        params.require(:pet).permit(:name, :species, :breed, :colors, :details, :street, :city, :country, :zip_code)
+    end
 end
