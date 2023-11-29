2e553b399640ca8098d69ac82ceb0210e69f10ee
Nivea Vilar <niveavilar@gmail.com>
2023-11-29 15:15:15 +0100

    methods and views for deleting and updating a pet

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,6 @@
 class PetsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
-
   def index
     if params[:missing] == "true"
       @pets = Pet.missing_pet
@@ -11,26 +10,43 @@ class PetsController < ApplicationController
       @pets = Pet.all
     end
   end
-  
+
   def show
-   @pet = Pet.find(params[:id])
+    @pet = Pet.find(params[:id])
   end
-  
+
   def new
     @pet = Pet.new
   end
-    
+
   def create
     @pet = Pet.new(pet_params)
     @pet.user = current_user
     if @pet.save
-       redirect_to pet_path(@pet)
+      redirect_to pet_path(@pet)
     else
-       render :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
-    
+
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    @pet.update(pet_params)
+    redirect_to pet_path(@pet)
+  end
+
+  def destroy
+    @pet = Pet.find(params[:id])
+    @pet.destroy
+    redirect_to root_path, status: :see_other
+  end
+
   private
+
   def pet_params
     params.require(:pet).permit(:name, :species, :breed, :colors, :details, :street, :city, :country, :zip_code)
   end
@@ -38,5 +54,5 @@ class PetsController < ApplicationController
   def pet_params
     params.require(:pet).permit(:photo)
   end
-
+  
 end
