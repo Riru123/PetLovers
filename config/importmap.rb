0449dfc574696ca5dc69782f9543ac391cc2d960
Riru Mizutani <seahorse3106@gmail.com>
2023-12-01 00:08:00 +0900

    I changed form

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
-pin "@rails/actioncable"
+pin "@rails/actioncable", to: "https://cdn.jsdelivr.net/npm/@rails/actioncable@7.1.2/app/assets/javascripts/actioncable.esm.js"
