be820d0644023c805e47d82a4bbedf3a2c11ba38
Rita Goncalves <ritamendesgoncalves@gmail.com>
2023-12-04 12:43:26 +0100

    Fixbug message styling

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,14 +7,15 @@ class MessagesController < ApplicationController
     if @message.save
       ChatroomChannel.broadcast_to(
         @chatroom,
-        render_to_string(partial: "message", locals: {message: @message})
+        message: render_to_string(partial: "message", locals: { message: @message }),
+        sender_id: @message.user.id
       )
       head :ok
     else
       render "chatrooms/show", status: :unprocessable_entity
     end
   end
-
+  
   private
 
   def message_params
