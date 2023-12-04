dbec8db5bbd429e047fd8f4742b592c82e5cafce
Robert Chheav <robert.chheav@gmail.com>
2023-12-04 14:59:33 +0100

    logic for the sender index works

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,9 @@
 class ChatroomsController < ApplicationController
+  def index
+    @chatrooms = current_user.chatrooms
+    @pets = Pet.all
+  end
+
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
