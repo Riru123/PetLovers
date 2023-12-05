54cc1e642201ed7554665a42b6db91f051b88ac0
Nivea Vilar <niveavilar@gmail.com>
2023-12-05 10:27:25 +0100

    making map method conciser

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,26 +7,16 @@ class PagesController < ApplicationController
   def home
     @pets_lost = Pet.where(category: "I lost").limit(10).order(created_at: :desc)
     @pets_found = Pet.where(category: "I found").limit(10).order(created_at: :desc)
-    @pets = Pet.all
-
-    @markers = @pets.map do |pet|
-      location = Mapbox::Geocoder.geocode_forward(pet.city)
-      if location[0]["features"].size > 0
-        coordinates = location[0]["features"][0]["geometry"]["coordinates"]
-        if coordinates
-          {
-            lat: coordinates[1],
-            lng: coordinates[0],
-            info_window_html: render_to_string(partial: "shared/info_window", locals: { pet: pet }),
-            marker_html: render_to_string(partial: "shared/marker", locals: { pet: pet })
-          }
-        end
-      end
-    end
-
+    @map_data = get_map_data
   end
 
   def map
+    @map_data = get_map_data
+  end
+
+  private
+
+  def get_map_data
     @pets = Pet.all
     @markers = @pets.map do |pet|
       location = Mapbox::Geocoder.geocode_forward(pet.city)
