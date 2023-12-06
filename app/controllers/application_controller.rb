b1527d9199cfcea3b8db8421b82e6b54e0b855c3
Rita Goncalves <ritamendesgoncalves@gmail.com>
2023-12-06 16:55:19 +0100

    Meta data for social media

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :phone])
   end
+
+  # app/controllers/application_controller.rb
+  def default_url_options  
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
